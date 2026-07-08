35c2084825f4e08b6bd65938002d43e662730e4c
Zeynep Aizpurvs <zey.ozdemir21@outlook.com>
2026-07-08 15:28:34 +0300

    required tasks are done.

--- src/main/java/com/accenture/springai_bootcamp_demo/config/OpenRouterProperties.java
+++ src/main/java/com/accenture/springai_bootcamp_demo/config/OpenRouterProperties.java
@@ -3,6 +3,6 @@ package com.accenture.springai_bootcamp_demo.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "openrouter")
-public record OpenRouterProperties(){
+public record OpenRouterProperties(String systemPrompt){
     //TODO: add OpenRouter config attributes
 }
